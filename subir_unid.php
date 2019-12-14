c6b366dd728be848ef5ef87504b0c7feb7aaffc2
SDavisco <42480979+SDavisco@users.noreply.github.com>
2019-12-14 13:45:09 -0500

    suvir datos

--- subir_unid.php
+++ subir_unid.php
@@ -1,13 +1,18 @@
 <?php
     require_once('bd/conexion.php');
+    $unidad_d = $_POST['unidad'];
     $docente = $_POST['docente'];
-    $docentes="INSERT INTO docente (docente) VALUES ('$docente')";
-    if (mysqli_query($conn, $docentes))
+    $sem_ac = $_POST['sem_ac'];
+    $sem_lec = $_POST['sem_lectivo'];
+    $turno = $_POST['turno'];
+    $unidades="INSERT INTO UNIDAD_DOCENTE (nomb_unidad, docente, semestre_academico, semestre_lectivo, turno)
+    VALUES ('$unidad_d','$docente','$sem_ac','$sem_lec','$turno')";
+    if (mysqli_query($conn, $unidades))
     {
         
     }
     else{
-        echo "error". $docentes."<br>".mysqli_error($conn);
+        echo "error". $unidades."<br>".mysqli_error($conn);
     }
     ?>
         <script language="javascript">
