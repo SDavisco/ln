503d60b3d2e74bea68c35fba2fad1a6dff116f0d
SDavisco <42480979+SDavisco@users.noreply.github.com>
2019-12-15 23:25:18 -0500

    gg

--- s_docente.php
+++ s_docente.php
@@ -10,6 +10,6 @@
         echo "error". $docentes."<br>".mysqli_error($conn);
     }
     ?>
-        <script language="javascript">
+    <script language="javascript">
     window.location="docentes.php";
     </script>
--- s_silabus.php
+++ s_silabus.php
@@ -1,16 +1,38 @@
-<<?php
+<?php
     require_once('bd/conexion.php');
-    if($_FILES["silabus"]["error"]>0;{
+    $id_insert = $conn->insert_id;
+    if($_FILES["silabus"]["error"]>0){
         echo "eror al cargar archivo";
-     }, else {
-         $permitidos = array("application/pdf")
-     }
+     } else {
+        $permitidos = array("application/pdf");
+        $limite_kb = 1000;
 
-     $limite_kb = 200;
+     if(in_array($_FILES["silabus"]["type"], $permitidos) && $_FILES["silabus"]["size"]<= $limite_kb * 1024){
+         $ruta = 'silabus/'.$id_insert.'/';
+         $silabus = $ruta.$_FILES["silabus"]["name"];
+         if(!file_exists($ruta)){
+             mkdir($ruta);
+         }
+     if (!file_exists($silabus)){
+         $resultado = @move_uploaded_file($_FILES["silabus"]["tmp_name"], $silabus);
+				
+         if($resultado){
+            echo "Archivo Guardado";
+            } else {
+            echo "Error al guardar archivo";
+        }
+        
+        } else {
+        echo "Archivo ya existe";
+    }
+    
+    } else {
+    echo "Archivo no permitido o excede el tamaño";
+}
 
-     if(in_array($_FILES["silabus"], $permitidos) && $_FILES["silabus"]["size"]<- $limite_kb * 1024) 
+}
 
-    /*$silabus = $_POST['silabus'];;
+/*$silabus = $_POST['silabus'];;
     $s_silabus="INSERT INTO silabus VALUES ('$docente')";
     if (mysqli_query($conn, $docentes))
     {
@@ -23,3 +45,25 @@
     <script language="javascript">
     window.location="docentes.php";
     </script>*/
+?>
+<html lang="es">
+	<head>
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="row" style="text-align:center">
+					<?php if($resultado) { ?>
+						<h3>REGISTRO GUARDADO</h3>
+						<?php } else { ?>
+						<h3>ERROR AL GUARDAR</h3>
+					<?php } ?>
+					
+					<a href="silabus.php" class="btn btn-primary">Regresar</a>
+					
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
