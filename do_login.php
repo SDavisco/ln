c813246a5f4a3cf9530b6970f87e402a6da35c0b
SDavisco <42480979+SDavisco@users.noreply.github.com>
2020-01-13 00:31:51 -0500

    vistas

--- do_login.php
+++ do_login.php
@@ -8,14 +8,21 @@
 		$rs = mysqli_query($conn, "SELECT * FROM acceso  WHERE user_name = '$usuario' AND user_pass = '$contra'");
 
 		if($rs->num_rows>0){
-			while($row = mysqli_fetch_array($rs)){
-				//echo "Bienvenido ".$row['user_name'];
-				@session_start();
+		  $row = mysqli_fetch_array($rs);
+		   if (in_array('1',$row, true)){
+			@session_start();
 				$_SESSION["user_name"]=$row["user_name"];
 				$_SESSION["user_code"]=$row["user_code"];
 				$_SESSION["user_type"]=$row["user_type"];
-				header("Location: index.php");
-			}
+				header("Location: index1.php");
+		   	}	
+		  if (in_array('2',$row, true)){
+			@session_start();
+				$_SESSION["user_name"]=$row["user_name"];
+				$_SESSION["user_code"]=$row["user_code"];
+				$_SESSION["user_type"]=$row["user_type"];
+				header("Location: index2.php");
+		   	}
 
 		}else{
 			echo "Usuario o contraseña no son válidos";
